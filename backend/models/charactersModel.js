5af9d8ea0a2bbd74d88ec5a9930f5a3f30141281
adisorotzkin <adisorotz@gmail.com>
2023-12-31 09:18:46 +0200

    adi29/12/23

--- backend/models/charactersModel.js
+++ backend/models/charactersModel.js
@@ -13,8 +13,10 @@ exports.CharactersModel = mongoose.model("characters", charactersSchema);
 
 exports.validCharacter = (_reqBody) => {
     let joiSchema = joi.object({
+        storyId: joi.string(),
         characterName: joi.string().min(2).max(99).required(),
-        description: joi.string().min(2).max(99).required()
+        description: joi.string().min(2).max(99).required(),
+        image: joi.string()
     })
     return joiSchema.validate(_reqBody);
 }
