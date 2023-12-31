fd032d45f32cde6f59c07c807e6add4f9e0b5bc1
Chana_K <chanbanrox@gmail.com>
2023-12-31 12:15:45 +0200

    search

--- frontend/vite/src/comps/imageGenerator.jsx
+++ frontend/vite/src/comps/imageGenerator.jsx
@@ -10,7 +10,7 @@ const ImageGenerator = () => {
     let inputRef = useRef(null)
     const [loading, setLoading] = useState(false)
     const { imageUrl, setImageUrl } = useContext(AppContext);
-
+    
 
 
 
--- frontend/vite/src/comps/newStory.jsx
+++ frontend/vite/src/comps/newStory.jsx
@@ -1,11 +1,12 @@
 import React, { useContext, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { apiService } from '../service/apisService';
-import ImageGenerator from './imageGenerator'
+import ImageGenerator from './imageGenerator';
 import { AppContext } from '../context/context';
-import '../comps_css/newStory.css'
-import Navbar from '../static_comps/navbar'
-
+import '../comps_css/newStory.css';
+import Navbar from '../static_comps/navbar';
+import axios from 'axios';
+import { storage } from '../../config/firebaseConfig';
 
 const NewStory = () => {
   const titleRef = useRef(null);
@@ -13,7 +14,33 @@ const NewStory = () => {
   const genreRef = useRef(null);
   const navigate = useNavigate();
   const { postAuthenticatedData } = apiService();
-  const { imageUrl, setImageUrl, genresArray } = useContext(AppContext);
+  const { imageUrl, genresArray } = useContext(AppContext);
+
+  const uploadImageToFirebase = async (imageUrl, storagePath) => {
+    try {
+      const response = await axios({
+        method: 'get',
+        url: imageUrl,
+        responseType: 'arraybuffer', // Set the responseType to 'arraybuffer'
+      });
+
+      const buffer = Buffer.from(response.data, 'binary'); // Convert arraybuffer to Buffer
+
+      const bucket = storage.bucket();
+      const file = bucket.file(storagePath);
+
+      await file.save(buffer, {
+        metadata: {
+          contentType: 'image/jpeg',
+        },
+      });
+
+      console.log('Image uploaded to Firebase Storage successfully.');
+    } catch (error) {
+      console.error('Error uploading image to Firebase Storage:', error);
+      throw error;
+    }
+  };
 
   const handleNext = async () => {
     const title = titleRef.current.value;
@@ -25,20 +52,22 @@ const NewStory = () => {
       return;
     }
 
+    const storagePath = `images/${title.toLowerCase().replace(/ /g, '_')}.jpg`;
 
+    await uploadImageToFirebase(imageUrl, storagePath);
+
+    const firebaseImageUrl = `https://storage.googleapis.com/${storage.bucket().name}/${encodeURIComponent(storagePath)}`;
 
     try {
       const response = await postAuthenticatedData('/stories', {
         title: title,
         description: description,
         genre: genre,
-        coverImg: imageUrl
+        coverImg: firebaseImageUrl,
       }, localStorage.getItem('token'));
 
-        console.log("Story created successfully!" , response);
-        navigate('/newParagraph', {state: {storyInfo: response }});
-     
-      
+      console.log('Story created successfully!', response);
+      navigate('/newParagraph', { state: { storyInfo: response } });
     } catch (error) {
       console.error('Error creating story:', error);
       alert('An error occurred while creating the story. Please try again.');
@@ -61,7 +90,7 @@ const NewStory = () => {
           </div>
           <div className='form-group'>
             <label htmlFor="genre">Genre *</label>
-            <select id="genre" className="form-control" ref={genreRef}>
+            <select id="genre" className="form-control mb-2" ref={genreRef}>
               <option value="" disabled selected>Select your genre</option>
               {genresArray.map((genre) => (
                 <option key={genre} value={genre}>
@@ -71,7 +100,7 @@ const NewStory = () => {
             </select>
           </div>
           <ImageGenerator />
-          <div className="btn-next-div">
+          <div className="btn-next-div mb-4">
             <button type="button" className="btn text-white" onClick={handleNext}><i className="fa fa-arrow-right btn-next" aria-hidden="true"></i></button>
           </div>
         </form>
