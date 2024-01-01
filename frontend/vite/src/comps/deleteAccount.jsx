1a33f4b4ed2a44ede475f3be1983876021f5c86c
adisorotzkin <adisorotz@gmail.com>
2024-01-01 16:12:00 +0200

    UPDATE

--- frontend/vite/src/comps/deleteAccount.jsx
+++ frontend/vite/src/comps/deleteAccount.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { apiService } from '../service/apisService';
+
 
 const DeleteAccount = () => {
+  const { userData, setUserData } = useContext(AppContext);
+  const userToken = localStorage.getItem('token');
+  const { updateAuthenticatedData } = apiService();
+
+  useEffect(async() => { 
+    const updatedUserInfo = {
+      username: userData.username,
+      email: userData.email,
+      bio: userData.bio,
+      profilePicture: userData.profilePicture,
+      dateOfBirth: userData.dateOfBirth,
+      registrationDate: userData.registrationDate, 
+      active : false
+    };
+    const updatedData = await updateAuthenticatedData('/users/',userData._id, updatedUserInfo, userToken);
+    
+  }, [])
+
   return (
-    <div>D</div>
+    <div></div>
   )
 }
 
