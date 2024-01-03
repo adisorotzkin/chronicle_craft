11352c35f11dd4c7cb99cec7f47b6ea56c224e45
adisorotzkin <adisorotz@gmail.com>
2024-01-03 10:40:54 +0200

    comit

--- frontend/vite/src/comps/addRateing.jsx
+++ frontend/vite/src/comps/addRateing.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { apiService } from '../service/apisService';
 import StarRating from './starRating';
+import '../comps_css/addRating.css';
 
 const AddRateing = () => {
     const location = useLocation();
@@ -20,23 +21,22 @@ const AddRateing = () => {
             const res = await postData('/ratings', { author: profileData._id, rating: rating });
             console.log(res);
 
-            // Handle success, e.g., show a success message to the user
         } catch (error) {
             console.error('Error while making API request:', error);
-            // Handle error, e.g., show an error message to the user
         }
     };
 
     return (
-        <div style={{ backgroundColor: 'black', color: 'white', padding: '20px', borderRadius: '10px' }}>
+        <div className='mainRatings' >
             <div className="profile-img-div col-3">
                 <img className="profile-img" src={profileData.profilePicture} alt="Profile" />
             </div>
-            <div>
-                <h2>Come vote and impact! <br />Rate {profileData.username}</h2>
-                <label htmlFor="rating">Rating:</label>
-                <input className='btn text-white border' type="number" id="rating" min="1" max="5" value={rating} onChange={handleRatingChange} />
-                <button className='btn text-white border' onClick={handleSubmit}>
+            <div className='divider'></div>
+            <div className='pt-5'>
+                <h2 className='h2ating'>Come vote and impact! <br />Rate {profileData.username}</h2>
+                <label className='labaelRating'>Rating:</label>
+                <input className='btn btn-dark text-white border' type="number" id="rating" min="1" max="5" value={rating} onChange={handleRatingChange} />
+                <button className='btn btn-dark text-white border btnRatings' onClick={handleSubmit}>
                     Submit Rating
                 </button>
             </div>
